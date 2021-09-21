9074109ffd1ebe93eb11b70611577e698f78e07f
Phillip Weitkamp <pweitkamp@outlook.de>
2021-09-21 22:57:11 +0200

    testing;

--- EnvironmentServer.DAL/Repositories/UsersRepository.cs
+++ EnvironmentServer.DAL/Repositories/UsersRepository.cs
@@ -93,7 +93,7 @@ namespace EnvironmentServer.DAL.Repositories
                 Command.Connection = connection;
                 Command.ExecuteNonQuery();
 
-                Command = new MySqlCommand("create user @username@'localhost' identified by @password;");
+                Command = new MySqlCommand("create user @username'@localhost' identified by @password;");
                 Command.Parameters.AddWithValue("@password", shellPassword);
                 Command.Parameters.AddWithValue("@username", user.Username);
                 Command.Connection = connection;
