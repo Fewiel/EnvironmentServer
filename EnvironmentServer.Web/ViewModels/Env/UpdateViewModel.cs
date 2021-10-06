d1a64aa550d2d01e7b45a8eead3c7044dcc49b79
p.weitkamp <info@p-weitkamp.de>
2021-10-06 16:35:52 +0200

    fix;

--- EnvironmentServer.Web/ViewModels/Env/UpdateViewModel.cs
+++ EnvironmentServer.Web/ViewModels/Env/UpdateViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +8,6 @@ namespace EnvironmentServer.Web.ViewModels.Env
 {
     public class UpdateViewModel
     {
-        [MinLength(4), MaxLength(32), DataType(DataType.Text)]
-        [RegularExpression(@"^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$", ErrorMessage = "Environment name not allowed")]
         public long ID { get; set; }
         public string EnvironmentName { get; set; }
         public string SWVersion { get; set; }
