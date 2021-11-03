0cf28e3afdf00fd6bb8445e740a14d9465441028
p.weitkamp <info@p-weitkamp.de>
2021-11-03 16:42:26 +0100

    start cronworker

---
+++ EnvironmentServer.Daemon/CronWorker.cs
@@ -0,0 +1,38 @@
+﻿using EnvironmentServer.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EnvironmentServer.Daemon
+{
+    public class CronWorker
+    {
+        private Database DB;
+        private readonly CancellationTokenSource cancellationToken;
+        private readonly Task ActiveWorkerTask;
+
+        public CronWorker(Database db)
+        {
+            DB = db;
+            cancellationToken = new CancellationTokenSource();
+            ActiveWorkerTask = Task.Factory.StartNew(DoWork, cancellationToken.Token);
+        }
+
+        public void StopWorker()
+        {
+            cancellationToken.Cancel();
+            Console.WriteLine("Waiting for last cron");
+            ActiveWorkerTask.Wait(TimeSpan.FromMinutes(15));
+        }
+
+        private async Task DoWork()
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+            }
+        }
+    }
+}
