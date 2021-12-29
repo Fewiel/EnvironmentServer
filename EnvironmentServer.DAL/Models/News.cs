3e1ba6eeddd4bf09ea6e083a1631e198d56372b2
Phillip Weitkamp <pweitkamp@outlook.de>
2021-12-29 01:10:06 +0100

    Add news page;

---
+++ EnvironmentServer.DAL/Models/News.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EnvironmentServer.DAL.Models;
+
+public class News
+{
+    public long ID { get; set; }
+    public long UserID { get; set; }
+    public string Content { get; set; }
+    public DateTime Created { get; set; }
+}
---
+++ EnvironmentServer.DAL/Repositories/NewsRepository.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using EnvironmentServer.DAL.Models;
+using System.Collections.Generic;
+
+namespace EnvironmentServer.DAL.Repositories;
+
+public class NewsRepository
+{
+    private Database DB;
+    public NewsRepository(Database db)
+    {
+        DB = db;
+    }
+
+    public IEnumerable<News> GetLatest()
+    {
+        using var connection = DB.GetConnection();
+        return connection.Query<News>("select * from `news` Order BY Created limit 10;");
+    }
+
+    public void Insert(News news)
+    {
+        using var connection = DB.GetConnection();
+        connection.Execute("INSERT INTO `news` (`Id`, `UserID`, `Content`, `Created`) " +
+            "VALUES (NULL, @uid, @content, CURRENT_TIMESTAMP);", new
+        {
+            uid = news.UserID,
+            content = news.Content
+        });
+    }
+
+    public void Update(News news)
+    {
+        using var connection = DB.GetConnection();
+        connection.Execute("UPDATE `news` SET `Content` = @content WHERE `news`.`Id` = @id;", new
+            {
+                id = news.ID,
+                content = news.Content
+            });
+    }
+}
