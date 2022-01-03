48e88498947432204099899752607d4d7dcd56b4
Phillip Weitkamp <pweitkamp@outlook.de>
2022-01-03 04:02:22 +0100

    Fix null refs;

--- EnvironmentServer.DAL/Models/UserInformation.cs
+++ EnvironmentServer.DAL/Models/UserInformation.cs
@@ -6,10 +6,10 @@ public class UserInformation
 {
     public long ID { get; set; }
     public long UserID { get; set; }
-    public string Name { get; set; }
-    public string SlackID { get; set; }
+    public string Name { get; set; } = "";
+    public string SlackID { get; set; } = "";
     public long DepartmentID { get; set; }
-    public string DepartmentName { get; set; }
+    public string DepartmentName { get; set; } = "";
     public DateTime? AbsenceDate { get; set; }
     public string AbsenceReason { get; set; } = "";
     public string AdminNote { get; set; } = "";
@@ -18,5 +18,8 @@ public class UserInformation
     {
         AdminNote ??= "";
         AbsenceReason ??= "";
+        Name ??= "";
+        SlackID ??= "";
+        DepartmentName ??= "";
     }
 }
