1a3dce4f0532d06144c21e9f110a9e26023ae57a
p.weitkamp <p.weitkamp@shopware.com>
2022-03-11 20:48:22 +0100

    debug

--- EnvironmentServer.Web/Startup.cs
+++ EnvironmentServer.Web/Startup.cs
@@ -47,7 +47,7 @@ namespace EnvironmentServer.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || File.Exists(".debug"))
             {
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
