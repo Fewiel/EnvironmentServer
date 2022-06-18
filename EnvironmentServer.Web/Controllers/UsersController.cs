755161887d84b2271dd43b7b11e6b6d27dcb6e81
Phillip Weitkamp <pweitkamp@outlook.de>
2022-06-19 01:24:17 +0200

    remove unused usings;

--- EnvironmentServer.Web/Controllers/UsersController.cs
+++ EnvironmentServer.Web/Controllers/UsersController.cs
@@ -6,10 +6,7 @@ using EnvironmentServer.Web.ViewModels.Login;
 using EnvironmentServer.Web.ViewModels.Users;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace EnvironmentServer.Web.Controllers
