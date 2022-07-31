2359362b3c046f3486df8c7aaf1abc473a8be319
Phillip Weitkamp <pweitkamp@outlook.de>
2022-07-31 02:15:55 +0200

    Rework Docker

---
+++ EnvironmentServer.DAL/Models/DockerContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EnvironmentServer.DAL.Models;
+
+public class DockerContainer
+{
+    public long ID { get; set; }
+    public string DockerID { get; set; }
+    public long UserID { get; set; }
+    public string Name { get; set; }
+    public long DockerComposeFileID { get; set; }
+    public bool Active { get; set; }
+    public DateTimeOffset LatestUse { get; set; }
+    public DateTimeOffset Created { get; set; }
+}
---
+++ EnvironmentServer.DAL/Repositories/DockerContainerRepository.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using EnvironmentServer.DAL.Models;
+using EnvironmentServer.DAL.Utility;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EnvironmentServer.DAL.Repositories;
+
+public class DockerContainerRepository
+{
+    private Database DB;
+
+    public DockerContainerRepository(Database db)
+    {
+        DB = db;
+    }
+
+    //Get all
+    public async Task<IEnumerable<DockerContainer>> GetAsync()
+    {
+        using var c = new MySQLConnectionWrapper(DB.ConnString);
+        return await c.Connection.QueryAsync<DockerContainer>("select * from `docker_containers`;");
+    }
+
+    //Get all for user
+    public async Task<IEnumerable<DockerContainer>> GetAllForUserAsync(long uid)
+    {
+        using var c = new MySQLConnectionWrapper(DB.ConnString);
+        return await c.Connection.QueryAsync<DockerContainer>("select * from `docker_containers` where UserID = @uid;", new
+        {
+            uid
+        });
+    }
+
+    //Get by ID
+    public async Task<IEnumerable<DockerContainer>> GetByIDAsync(long id)
+    {
+        using var c = new MySQLConnectionWrapper(DB.ConnString);
+        return await c.Connection.QueryAsync<DockerContainer>("select * from `docker_containers` where ID = @id;", new
+        {
+            id
+        });
+    }
+
+    //Get by File
+    public async Task<IEnumerable<DockerContainer>> GetByFileIDAsync(long id)
+    {
+        using var c = new MySQLConnectionWrapper(DB.ConnString);
+        return await c.Connection.QueryAsync<DockerContainer>("select * from `docker_containers` where DockerComposeFileID = @id;", new
+        {
+            id
+        });
+    }
+}
