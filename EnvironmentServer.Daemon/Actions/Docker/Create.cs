a09e465bdd15f095a34ab47fb9adafc8a572bccf
Phillip Weitkamp <pweitkamp@outlook.de>
2022-08-07 15:13:25 +0200

    add Docker File Builder

--- EnvironmentServer.Daemon/Actions/Docker/Create.cs
+++ EnvironmentServer.Daemon/Actions/Docker/Create.cs
@@ -2,12 +2,9 @@
 using Ductus.FluentDocker.Services;
 using EnvironmentServer.DAL;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using Ductus.FluentDocker.Model.Containers;
+using Ductus.FluentDocker.Builders;
 using System.Linq;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using EnvironmentServer.DAL.Models;
 
 namespace EnvironmentServer.Daemon.Actions.Docker
 {
@@ -15,30 +12,29 @@ namespace EnvironmentServer.Daemon.Actions.Docker
     {
         public override string ActionIdentifier => "docker.start";
 
-        public override Task ExecuteAsync(ServiceProvider sp, long variableID, long userID)
+        public override Task ExecuteAsync(ServiceProvider db, long variableID, long userID)
         {
-            var hosts = new Hosts().Discover();
-            var _docker = hosts.FirstOrDefault(x => x.IsNative) ?? hosts.FirstOrDefault(x => x.Name == "default");
-            var db = sp.GetService<Database>();
-
-            var config = JsonConvert.DeserializeObject<DockerInstance>(db.CmdActionDetail.Get(variableID).JsonString);
-
-            if (_docker == null)
-            {
-                db.Logs.Add("docker.start", "Docker not found on Host");
-                return Task.CompletedTask;
-            }
-
-            var id = _docker.Host.Run(config.Image, new ContainerCreateParams
-            {
-                Name = config.Name,
-                PortMappings = config.PortMappings,
-                Environment = config.DockerEnvironment,
-                Interactive = config.Interactive
-            }, _docker.Certificates).Data;
-
-            config.ID = id;
-
+            throw new System.NotImplementedException();
         }
+
+        //public override Task ExecuteAsync(ServiceProvider sp, long variableID, long userID)
+        //{
+        //    var hosts = new Hosts().Discover();
+        //    var _docker = hosts.FirstOrDefault(x => x.IsNative) ?? hosts.FirstOrDefault(x => x.Name == "default");
+        //    var db = sp.GetService<Database>();
+
+        //    if (_docker == null)
+        //    {
+        //        db.Logs.Add("docker.start", "Docker not found on Host");
+        //        return Task.CompletedTask;
+        //    }
+
+        //    var svc = new Builder()
+        //                .UseContainer()
+        //                .UseCompose()
+        //                .FromFile(file)
+        //                .RemoveOrphans()
+        //                .Build().Start())
+        //}
     }
 }
---
+++ EnvironmentServer.Daemon/Utility/DockerFileBuilder.cs
@@ -0,0 +1,55 @@
+﻿using EnvironmentServer.Daemon.Models;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace EnvironmentServer.Daemon.Utility;
+
+public static class DockerFileBuilder
+{
+    private static Regex Regex = new("\\$port:([A-Za-z]*)");
+
+    public static DockerFileResult Build(string template, List<int> usedPorts, int minPort)
+    {
+        var matches = Regex.Matches(template);
+        var result = new DockerFileResult();
+
+        foreach (Match m in matches)
+        {
+            var port = GetPort(usedPorts, minPort);
+
+            result.AddPort(m.Groups[1].Value, port);
+
+            template = template.Replace(m.Groups[0].Value, port.ToString());
+        }
+
+        result.Content = template;
+        return result;
+    }
+
+    private static int GetPort(List<int> usedPorts, int minPort)
+    {
+        if (usedPorts.Count == 0)
+        {
+            usedPorts.Add(minPort);
+            return minPort;
+        }
+
+        for (int i = 0; i < usedPorts.Count; i++)
+        {
+            if (i == usedPorts.Count - 1)
+            {
+                usedPorts.Add(usedPorts[i] + 1);
+                return usedPorts[i] + 1;
+            }
+
+            if (usedPorts[i] + 1 != usedPorts[i + 1])
+            {
+                usedPorts.Insert(i + 1, usedPorts[i] + 1);
+                return usedPorts[i] + 1;
+            }
+        }
+
+        throw new InvalidOperationException();
+    }
+}
