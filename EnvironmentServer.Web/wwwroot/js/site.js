58376da4ef10c8b35bdb33c8b801b6a25569d583
p.weitkamp <p.weitkamp@shopware.com>
2022-10-13 16:40:22 +0200

    Simplify promt

--- EnvironmentServer.Web/wwwroot/js/site.js
+++ EnvironmentServer.Web/wwwroot/js/site.js
@@ -19,33 +19,12 @@ const CopyToClipboard = (text) => {
 };
 
 function AreYouSurePromt() {
-    return new Promise(function (resolve, reject) {
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonText: 'Yes, do it!',
-            cancelButtonText: 'No, cancel!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                Swal.fire(
-                    'Done!',
-                    'There is no more turning back...',
-                    'success'
-                )
-                resolve();
-            } else if (
-                /* Read more about handling dismissals below */
-                result.dismiss === Swal.DismissReason.cancel
-            ) {
-                Swal.fire(
-                    'Cancelled',
-                    'Maybe next time...',
-                    'error'
-                )
-                reject();
-            }
-        });
-    })
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, do it!',
+        cancelButtonText: 'No, cancel!'
+    });
 };
