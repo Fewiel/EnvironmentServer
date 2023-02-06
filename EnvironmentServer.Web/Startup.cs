f2b16ec6605bf736e1d96c12e09fb8542aec5ffa
p.weitkamp <p.weitkamp@shopware.com>
2023-02-06 14:30:54 +0100

    security fix

--- EnvironmentServer.Web/Startup.cs
+++ EnvironmentServer.Web/Startup.cs
@@ -63,6 +63,14 @@ public class Startup
             app.UseExceptionHandler("/Home/Error");
         }
 
+        app.Use(async (context, next) =>
+        {
+            context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+            context.Response.Headers.Add("X-Frame-Options", "DENY");
+            context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
+            await next();
+        });
+
         //app.UseHttpsRedirection();
         app.UseStaticFiles();
         app.UseSession();
