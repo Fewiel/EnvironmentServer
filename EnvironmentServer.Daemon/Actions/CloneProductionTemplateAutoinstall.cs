6e49322864bac2d0105667ae81f4c40ec610e345
p.weitkamp <p.weitkamp@shopware.com>
2023-05-26 14:19:40 +0200

    fix bash

--- EnvironmentServer.Daemon/Actions/CloneProductionTemplateAutoinstall.cs
+++ EnvironmentServer.Daemon/Actions/CloneProductionTemplateAutoinstall.cs
@@ -32,7 +32,7 @@ namespace EnvironmentServer.Daemon.Actions
             if (swVersion.Major == 4 && swVersion.Minor >= 18 || swVersion.Major == 5)
             {
                 Directory.Delete(homeDir, true);
-                await Bash.CommandAsync($"shopware-cli project create {homeDir} {version}", homeDir);
+                await Bash.CommandAsync($"shopware-cli project create {homeDir} {version}");
             }
             else
             {
