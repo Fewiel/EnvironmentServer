2474b0414c3c5cadb03860a36596e96d577e893b
p.weitkamp <p.weitkamp@shopware.com>
2023-05-26 14:13:16 +0200

    fix if

--- EnvironmentServer.Daemon/Actions/CloneProductionTemplateAutoinstall.cs
+++ EnvironmentServer.Daemon/Actions/CloneProductionTemplateAutoinstall.cs
@@ -29,7 +29,7 @@ namespace EnvironmentServer.Daemon.Actions
 
             var swVersion = new ShopwareVersion(version);
 
-            if (swVersion.Major >= 4 && swVersion.Minor >= 18)
+            if (swVersion.Major == 4 && swVersion.Minor >= 18 || swVersion.Major == 5)
             {
                 await Bash.CommandAsync($"shopware-cli project create {homeDir} {version}", homeDir);
             }
